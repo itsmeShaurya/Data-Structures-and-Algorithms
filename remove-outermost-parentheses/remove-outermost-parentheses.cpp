2fdc0698a59e45aae2e5ccbaeb396886fb27fda8
Shaurya pratap singh <shauryapratap251@gmail.com>
2023-06-01 19:36:28 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (78.62%) - LeetSync

---
+++ remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res;
+        int count  = 0;
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '('){
+                if(count > 0){
+                    res += s[i];
+                }
+                count++;
+            }
+            else{
+                if(count > 1){
+                    res += s[i];
+                }
+                count--;
+            }
+        }
+        return res;
+    }
+};
